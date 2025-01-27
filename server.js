3acbdc72fcf4b0ac9b42d698d78366b99b3cb672
MohamedAntar <aboantar852003@gmail.com>
2025-01-27 22:22:07 +0200

    x

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app= express();
 app.get('/', (req,res)=>{
     res.send('this is the output');
 });
+app.get('/anothermsg', (req,res)=>{
+    res.send('this is another message');
+});
 
 app.listen(5000, ()=>{
     console.log("App listing to http://localhost:5000");
